7294cb06a93cffbc2fb0d0d1a9cfe28cbb193320
rijj1 <rijwan9365480654@gmail.com>
2024-08-18 12:27:35 +0530

    Authentication System using Breeze initiated

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,23 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use PHPUnit\Framework\Attributes\PostCondition;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/posts/trash', [PostController:: class, 'trashed'])->name('posts.trashed');
 Route::get('post/{id}/restore', [PostController::class,'restore'])->name('post.restore');
